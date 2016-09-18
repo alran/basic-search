8aebbfdc56cc3eafc83d76ba9c6ee76e04481af8
Alyssa Ransbury <aransbury@gmail.com>
2016-09-18 12:49:42 -0400

    Remove comments

--- lib/sentence.rb
+++ lib/sentence.rb
@@ -1,16 +1,13 @@
 class Sentence
 
   attr_accessor :text, :search_ranking
-  # attr_reader :matching_words
 
   def initialize()
     @text = ''
-    # @matching_words = []
     @search_ranking = 0
   end
 
   def update_ranking
-    # @matching_words << word
     @search_ranking += 1
   end
 end
